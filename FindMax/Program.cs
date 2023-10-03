8ede7ccc20484d7ad6307b483cc622df68431fef
Артём Белоусов <belousov@psksk.ru>
2023-10-03 13:37:00 +0300

    Добавьте файлы проекта.

---
+++ FindMax/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[15];
+            Random random = new Random();            
+            for (int i = 0; i < 15; i++)
+            {
+                array[i] = random.Next(0, 51);
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            int max = array[0];
+            int min = array[0];
+            for (int i = 1; i < 15; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+                else if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            int sum = max + min;
+
+            Console.WriteLine("Сумма максимального и минимального элементов: {0}",sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sorting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 7;
+            int[] array = new int[n];
+            Console.WriteLine("Введите {0} чисел:", n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Введите число {0}: ", i + 1);
+                int number = int.Parse(Console.ReadLine());
+                array[i] = number;
+
+            }
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += array[i];
+            }
+            double average = sum / n;
+                        
+            Console.WriteLine("Среднее арифметическое: {0}", average);
+
+            Console.ReadKey();
+        }
+    }
+}
